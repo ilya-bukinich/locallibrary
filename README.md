956be155b4b62226c0d462b2d269db0a8969d438
ilya-bukinich <ilya.bukinich@gmail.com>
2020-03-05 12:13:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# locallibrary
+ Training project in Django Web Framework according to instructions from the portal MDN
---
+++ catalog/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
+
+admin.site.register(Genre)
+admin.site.register(Language)
+
+
+class BooksAuthorInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksAuthorInline]
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    list_filter = ('author', 'genre')
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        ('Book Instance', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
